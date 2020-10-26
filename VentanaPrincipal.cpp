1fc6e889ed35bfa63eebf69770552bdd9c0d2881
crespuig <hectorcrespuig@gmail.com>
2020-10-26 15:02:21 +0100

    Nuevas funciones

--- VentanaPrincipal.cpp
+++ VentanaPrincipal.cpp
@@ -1,5 +1,21 @@
 #include "VentanaPrincipal.h"
+#include <QStatusBar>
+#include <QMenuBar>
+#include <QMessageBox>
 
 VentanaPrincipal::VentanaPrincipal(QWidget *parent):QMainWindow(parent){
+    
+    editorCentral = new QTextEdit(this);
+    setCentralWidget(editorCentral);
+
+    QMenuBar *barra = new QMenuBar();
+    QMenu * fileMenu = barra->addMenu(QString("File"));
+
+    accionSalir = new QAction("Salir", this);
+    fileMenu->addAction(accionSalir);
+
+    setWindowIcon(QIcon("./images/icon.png"));
+
+    
 
 }
--- VentanaPrincipal.h
+++ VentanaPrincipal.h
@@ -1,13 +1,17 @@
 #ifndef VENTANAPRINCIPAL_H
 #define VENTANAPRINCIPAL_H
 #include <QMainWindow>
+#include <QTextEdit>
 
 class VentanaPrincipal : public QMainWindow{
 Q_OBJECT
 
 public:
     VentanaPrincipal(QWidget *parent = 0);
-        
+    
+private:
+    QTextEdit * editorCentral;
+    QAction * accionSalir;
 };
 
 
